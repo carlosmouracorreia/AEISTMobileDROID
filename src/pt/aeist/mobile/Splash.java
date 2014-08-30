ebdee09032c169a7ca952f053e2450415a4c89b8
Carlos Correia <carloscorreia94@gmail.com>
2014-08-30 11:40:51 +0100

    Splash Screen Final
    v0.02

--- src/pt/aeist/mobile/Splash.java
+++ src/pt/aeist/mobile/Splash.java
@@ -3,16 +3,11 @@ package pt.aeist.mobile;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class Splash extends Activity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        
-        
+        super.onCreate(savedInstanceState);     
         setContentView(R.layout.splash);
         
 
