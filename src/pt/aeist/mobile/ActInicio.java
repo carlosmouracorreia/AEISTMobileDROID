98690392f4f97462fbab8ee7f12baa68a2e43480
Carlos Correia <pm.correia.carlos@gmail.com>
2015-03-09 11:48:33 +0000

    minor UTF8 changes

--- src/pt/aeist/mobile/ActInicio.java
+++ src/pt/aeist/mobile/ActInicio.java
@@ -18,7 +18,7 @@ public class ActInicio extends ActionBarActivity implements
 	private ViewPager viewPager;
 	private TabsPagerAdapter mAdapter;
 	private ActionBar actionBar;
-	private String[] tabs = { "Eventos", "ServiÃ§os", "A AEIST" };
+	private String[] tabs = { "Eventos", "Serviços", "A AEIST" };
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
